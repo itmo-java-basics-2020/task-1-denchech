22a2278410e5be92bb7bec070cbb0bb0d1502f2f
denchech <d_cher00@mail.ru>
2020-02-17 16:21:57 +0300

    Stupid changes (I don't know how to work with it)

--- src/main/java/ru/itmo/java/Task1.java
+++ src/main/java/ru/itmo/java/Task1.java
@@ -6,7 +6,12 @@ public class Task1 {
      * @return 42
      */
     int get42() {
-        return 42;
+        //Let's do some changes to have this freaking system working
+        if (true)
+            return 42;
+        else{
+            return 42;
+        }
     }
 
 }
